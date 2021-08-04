01acc5afafd09f5386d518734e2f653881b81e33
Charlie Tharas <charlie@charliemax.dev>
2021-08-04 18:26:10 -0400

    Swapped main infrastructure over to Pandas DataFrame
    End result is the same, but all internal core functions now use non-SQL
    formats (dicts, collections.Counters, DataFrames)
    
    Killed verbose logging functions (they will be back)
    
    Durations do not work currently as they have been left on the old SQL
    format and will have to be converted later.

--- main.py
+++ main.py
@@ -5,36 +5,34 @@ import getopt
 import json
 import requests
 import re
+import pandas as pd
+import collections
     
 class Analyzer():
     
     # backbone methods
-    def __init__(self, verbose, duration, more_details, use_songs, analyze_year, apikey):
-        self.verbose, self.duration, self.more_details, self.use_songs, self.analyze_year, self.apikey = (verbose, duration, more_details, use_songs, analyze_year, apikey)
+    def __init__(self, verbose, duration, more_details, use_songs, analyze_year, apikey, load, loadfp):
+        self.verbose, self.duration, self.more_details, self.use_songs, self.analyze_year, self.apikey, self.load, self.loadfp = (verbose, duration, more_details, use_songs, analyze_year, apikey, load, loadfp)
         self.log = open('log.dat', 'w', encoding="utf8")
         self.main()
         
-    def main(self):
-        conn = sqlite3.connect('ytmusic.db')
-        self.cursor = conn.cursor()
-        with open('schema.sql') as self.fp:
-            self.cursor.executescript(self.fp.read())
-        self.data = ""
+    def main(self):    
+        self.file = self.open_file(sys.argv[1], ".json")
     
-        self.file = self.open_file()
-    
-        print("Welcome to YouTube Music Year Wrapper.")
-        print("We are now processing your file.")
-    
-        self.parse_json()
-        print("Removing duplicates")
-        self.delete_duplicate()
+        print("Welcome to YouTube Music Year Wrapper v3.0")
+        if self.load:
+            print("Loading your preprocessed history file")
+            self.songsDF = pd.read_csv(self.open_file(self.loadfp), ".csv")
+        else:
+            print("We are now processing your file")
+            self.parse_json()
+            print("Removing duplicates")
+            self.delete_duplicate()
     
         if self.verbose:
             self.print_db()
         if self.duration:
             print("Getting durations. This may take a while.")
-            self.data = self.get_duration()
         print("Getting top 10's")
         self.prepare_tops()
         if self.verbose:
@@ -52,108 +50,74 @@ class Analyzer():
             return True
         return False
     
-    def open_file(self):
-        if (sys.argv[1].endswith('.json')):
+    def open_file(self, filepath, filetype):
+        if (filepath.endswith(filetype)):
             try:
-                file = open(sys.argv[1], "r", encoding="utf8")
+                file = open(filepath, "r", encoding="utf8")
                 return file
             except:
                 print("Could not open your history file")
                 sys.exit()
         else:
-            print("Your history file should be a json file")
+            print("Your history file should be a", filetype, "file")
             sys.exit()
     
     def parse_json(self):
+        self.songs = {"Title": [], "Artist": [], "Year": [], "URL": []}
+        
         json_object = json.load(self.file)
         for obj in json_object:
             if (self.should_not_ignore(obj['title'], obj['time'], obj['header'])):
                 if ('subtitles' in obj):
-                    self.cursor.execute("""INSERT INTO songs(title, artist, year, url) VALUES(?, ?, ?, ?)""", (
-                        obj['title'][8:], obj['subtitles'][0]['name'], obj['time'], obj['titleUrl'][32:]))
-                elif (('titleUrl' in obj) and (self.duration)):
-                    self.cursor.execute("""INSERT INTO songs(title, artist, year, url) VALUES(?, ?, ?, ?)""", (
-                        "parseme", "parseme", obj['time'], obj['titleUrl'][32:]))
+                    self.songs["Title"].append(obj['title'][8:])
+                    self.songs["Artist"].append(obj['subtitles'][0]['name'].replace('- Topic ', '').replace('- Topic', ''))
+                    self.songs["Year"].append(obj['time'])
+                    self.songs["URL"].append(obj['titleUrl'][32:])
     
     def print_db(self):
-        # Print results from DB
-        print("####################Full List#####################", file=self.log)
-        self.cursor.execute("""SELECT id, artist, title, url, year FROM songs""")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            datetime.datetime.now()
-            print(
-                '{0} : {1} - {2} - {4} - {3}'.format(row[0], row[1], row[2], row[3], row[4]), file=self.log)
-        print("####################Non-Duplicate List#####################", file=self.log)
-        self.cursor.execute("""SELECT id, artist, title, url, occurence FROM report""")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            datetime.datetime.now()
-            print(
-                '{0} : {1} - {2} - {3} - {4}'.format(row[0], row[1], row[2], row[3], row[4]), file=self.log)
+        # VERBOSE FUNCTION TBA
+        pass
     
     def prepare_tops(self):
-        # Artist top
-        self.cursor.execute("""SELECT artist FROM report GROUP BY artist""")
-        result = self.cursor.fetchall()
-        for res in result:
-            occurences = 0
-            total_duration = 0
-            self.cursor.execute(
-                """SELECT occurence, duration FROM report WHERE artist = ?""", (res[0],))
-            artocc = self.cursor.fetchall()
-            for occ in artocc:
-                occurences += occ[0]
-                total_duration += occ[1]
-            self.cursor.execute("""INSERT INTO artist_count(artist, occurence, duration) VALUES(?, ?, ?)""",
-                           (res[0], occurences, total_duration))
-    
-            # Song Top
-        self.cursor.execute(
-            """SELECT title, artist, occurence FROM report GROUP BY url""")
-        result_song = self.cursor.fetchall()
-        for res_song in result_song:
-            self.cursor.execute("""INSERT INTO songs_count(title, artist, occurence) VALUES(?, ?, ?)""",
-                           (res_song[0], res_song[1], res_song[2]))
+        # Top 10 Songs
+        self.songs_top = self.songsDF.nlargest(10, ['Occurrences'])
+        print("--- TOP SONGS ---")
+        print(self.songs_top)
+        # Top 10 Artists
+        occurrences = {}
+        for i, v in enumerate(self.songsDF['Artist']):
+            try:
+                occurrences[v] += self.songsDF['Occurrences'][i]
+            except:
+                print(i, v)
+                occurrences[v] = self.songsDF['Occurrences'][i]
+        self.artists = {"Artist": occurrences.keys(), "Occurrences": occurrences.values()}
+        del occurrences
+        self.artistsDF = pd.DataFrame(self.artists)
+        self.artistsDF.to_csv("report-artists.csv")
+        self.artists_top = self.artistsDF.nlargest(10, ['Occurrences'])
+
+        print("--- TOP ARTISTS ---")
+        print(self.artists_top)
     
     def delete_duplicate(self):
-        # Doublon Deletor
-        self.cursor.execute(
-            """SELECT title, COUNT(*), artist, url FROM songs GROUP BY url""")
-        result_doublon = self.cursor.fetchall()
-        for res_doublon in result_doublon:
-            self.cursor.execute("""INSERT INTO report(title, artist, occurence, url, duration) VALUES(?, ?, ?, ?, 0)""",
-                           (res_doublon[0], res_doublon[2], res_doublon[1], res_doublon[3]))
-        self.cursor.execute(
-            """SELECT id, artist, title, url FROM report WHERE title = 'parseme'""")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            self.cursor.execute(
-                """SELECT artist, title FROM songs WHERE url = ? AND title != ?""", (row[3], "parseme"))
-            match = self.cursor.fetchone()
-            if match:
-                self.cursor.execute(
-                    """UPDATE report SET artist = ?, title = ? WHERE id = ?""", (match[0], match[1], row[0]))
-        if not self.duration:
-            self.cursor.execute("""DELETE FROM report WHERE title = 'parseme'""")
+        occurrences = collections.Counter(self.songs['URL'])
+        self.songs['Occurrences'] = []
+        for i in self.songs['URL']:
+            self.songs['Occurrences'].append(occurrences[i])
+        del occurrences
+        
+        self.historyDF = pd.DataFrame(self.songs)
+        self.songsDF = pd.DataFrame(self.songs)
+        self.total_songs = len(self.songsDF)
+        self.songsDF.drop_duplicates(subset=['URL'], inplace=True)
+        self.unique_songs = len(self.songsDF)
+        self.songsDF = self.songsDF.reset_index(drop=True)
+        self.songsDF.to_csv("report-songs.csv")
     
     def print_full_tops(self):
-        print("####################Top Artists#####################", file=self.log)
-        self.cursor.execute(
-            """SELECT artist, occurence FROM artist_count ORDER by occurence DESC""")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            datetime.datetime.now()
-            print('{0} - {1}'.format(row[0], row[1]), file=self.log)
-    
-        print("####################Top Songs#####################", file=self.log)
-        self.cursor.execute(
-            """SELECT artist, title, occurence FROM songs_count ORDER by occurence DESC""")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            datetime.datetime.now()
-            print('{0} - {1} - {2}'.format(row[0], row[1], row[2]), file=self.log)
-    
+        # VERBOSE FUNCTION TBA
+        pass
     
     # API management functions
     def parse_duration(self, duration):
@@ -239,37 +203,39 @@ class Analyzer():
         print("""<!DOCTYPE html><html><head><title>Wrapped</title><style type="text/css">body{background-color: #000000;}.center-div{position: absolute; margin: auto; top: 0; right: 0; bottom: 0; left: 0; width: 50%; height: 90%; background-color: #000000; border-radius: 3px; padding: 10px;}.ytm_logo{width: 15%;position: relative;top: 30px;left: 40px;}.title_logo{width: 30%;position: relative;top: 30px;left: 60px;}.right_title{position: absolute;font-family: "Product Sans";top: 55px;right: 10%;font-size: 2em;color: #ffffff;}.container{position: relative;top: 13%;left: 53px;}.minutes_title{font-family: "Product Sans";font-size: 2em;color: #ffffff;}.minutes{font-family: "Product Sans";font-size: 6em;color: #ffffff;}.row{display: flex;}.column{flex: 50%;}.list{font-family: "Roboto";font-size: 1.5em;line-height: 30px;color: #ffffff;}</style></head><body><div class="center-div"><img src="ytm_logo.png" class="ytm_logo"><img src="title.png" class="title_logo"/><span class="right_title">""", file=htmlreport)
         print(str(self.analyze_year), file=htmlreport)
         print(""" Wrapped</span><div class="container"><div class="minutes_title">Minutes Listened</div><div class="minutes">""", file=htmlreport)
-        if duration:
-            print(str(self.data[0]//60), file=htmlreport)
+        if self.duration:
+            print(str(self.total_minutes//60), file=htmlreport)
         else:
             print("N/A", file=htmlreport)
         print("""</div><br><br><div class="row"><div class="column"><div class="minutes_title">Top Artists</div><div class="list">""", file=htmlreport)
-        if duration:
-            self.cursor.execute("""SELECT artist, occurence, duration FROM artist_count WHERE (occurence > 5) ORDER by duration DESC LIMIT 10""")
+        if self.duration:
+            #TBA LAZY
+            #self.cursor.execute("""SELECT artist, occurence, duration FROM artist_count WHERE (occurence > 5) ORDER by duration DESC LIMIT 10""")
+            pass
         else:
-            self.cursor.execute("""SELECT artist, occurence, duration FROM artist_count WHERE (occurence > 5) ORDER by occurence DESC LIMIT 10""")
-        rows = self.cursor.fetchall()
-        for row in rows:
+            sorted_artists = self.artists_top
+            top_artists, top_occurrences = sorted_artists['Artist'], sorted_artists['Occurrences']
+            durations = [0] * 10
+            del sorted_artists
+        for i, j, v in zip(top_artists, top_occurrences, durations):
             print("<br>", file=htmlreport)
             if self.more_details:
-                if duration:
-                    print('{0} - {1} songs ({2} mins)'.format(str(row[0]).replace(' - Topic', ''), row[1], str(row[2]//60)), file=htmlreport)
+                if self.duration:
+                    #print('{0} - {1} songs ({2} mins)'.format(str(row[0]).replace(' - Topic', ''), row[1], str(row[2]//60)), file=htmlreport)
+                    pass
                 else:
-                    print('{0} - {1} songs'.format(str(row[0]).replace(' - Topic', ''), row[1]), file=htmlreport)
+                    print('{0} - {1} songs'.format(str(i), j), file=htmlreport)
             else:
-                print('{0}'.format(str(row[0]).replace(
-                    ' - Topic', '')), file=htmlreport)
+                print('{0}'.format(i), file=htmlreport)
         print("""</div></div><div class="column"><div class="minutes_title">Top Songs</div><div class="list">""", file=htmlreport)
-        self.cursor.execute(
-            """SELECT artist, title, occurence FROM songs_count ORDER by occurence DESC LIMIT 10""")
-        rows = self.cursor.fetchall()
-        for row in rows:
+        sorted_songs = self.songs_top
+        top_songs, top_artists, top_occurrences = sorted_songs['Title'], sorted_songs['Artist'], sorted_songs['Occurrences']
+        for i, j, k in zip(top_songs, top_artists, top_occurrences):
             print("<br>", file=htmlreport)
             if self.more_details:
-                print('{0} - {1} - {2} plays'.format(str(row[0]).replace(
-                    ' - Topic', ''), row[1], row[2]), file=htmlreport)
+                print('{0} - {1} - {2} plays'.format(j, i, k), file=htmlreport)
             else:
-                print('{0}'.format(row[1]), file=htmlreport)
+                print('{0} - {1}'.format(j, i), file=htmlreport)
         print("""</div></div></div></div></div></body></html>""", file=htmlreport)
         htmlreport.close()
     
@@ -278,33 +244,32 @@ class Analyzer():
         report = open('report_{0}.dat'.format(
             str(self.analyze_year)), 'w', encoding=("utf8"))
         print("#################### Top Artists #####################", file=report)
-        self.cursor.execute(
-            """SELECT artist, occurence FROM artist_count ORDER by occurence DESC""")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            datetime.datetime.now()
-            print('{0} - {1}'.format(row[0], row[1]), file=report)
+        # select artist, occurrence
+        sorted_artists = self.artistsDF.sort_values(by=['Occurrences'], ascending=False)
+        top_artists, top_occurrences = sorted_artists['Artist'], sorted_artists['Occurrences']
+        del sorted_artists
+        for i, j in zip(top_artists, top_occurrences):
+            print('{0} - {1}'.format(i, j), file=report)
     
         print("#################### Top Songs #####################", file=report)
-        self.cursor.execute(
-            """SELECT artist, title, occurence FROM songs_count ORDER by occurence DESC""")
-        rows = self.cursor.fetchall()
-        for row in rows:
+        # select artist, title, occurrence
+        sorted_songs = self.songsDF.sort_values(by=['Occurrences'], ascending=False)
+        top_songs, top_artists, top_occurrences = sorted_songs['Title'], sorted_songs['Artist'], sorted_songs['Occurrences']
+        for i, j, k in zip(top_songs, top_artists, top_occurrences):
             datetime.datetime.now()
-            print('{0} - {1} - {2}'.format(row[0], row[1], row[2]), file=report)
+            print('{0} - {1} - {2}'.format(j, i, k), file=report)
     
-        if duration:
+        if self.duration:
+            total_duration, total_songs = 0, len(self.historyDF) # TBA
+            # WILL ADD MORE STUFF AS WELL
             print("\n#################### Duration #####################", file=report)
-            print('Total duration : {0}'.format(self.data[0]), file=report)
-            print('Total song count : {0}'.format(self.data[2]), file=report)
-            print('Error count : {0}'.format(self.data[1]), file=report)
-            print('Error rate : {0}%'.format(
-                (float(self.data[1])/self.data[2])*100), file=report)
+            print('Total duration : {0}'.format(total_duration), file=report)
+            print('Total song count : {0}'.format(total_songs), file=report)
         report.close()
         self.gen_html_report()
     
 # flags
-verbose, more_details, duration, analyze_year, use_songs, apikey = (False, False, False, False, datetime.datetime.now().year, None)
+verbose, more_details, duration, analyze_year, use_songs, apikey, load, loadfp = (False, False, False, False, datetime.datetime.now().year, None, False, None)
 
 opts, args = getopt.getopt(sys.argv[2:], "d:y:mv", ["duration=", "year="])
 for o, token in opts:
@@ -319,5 +284,8 @@ for o, token in opts:
         analyze_year = token
     elif o in ("-s", "--songs"):
         use_songs = True
+    elif o in ("-l", "--load"):
+        load = True
+        loadfp = token
         
-analyzer = Analyzer(verbose, more_details, duration, analyze_year, use_songs, apikey)
+analyzer = Analyzer(verbose, duration, more_details, analyze_year, use_songs, apikey, load, loadfp)
