8170e840ac40db2f93f09f4a0e95d649d3de3dfc
Charlie Tharas <charlie@charliemax.dev>
2021-09-28 18:24:08 -0400

    Added TODOs as comments
    Save song history to CSV for better debug

--- main.py
+++ main.py
@@ -12,14 +12,18 @@ class Analyzer():
     
     # backbone methods
     def __init__(self, verbose, duration, more_details, use_songs, analyze_year, apikey, load, loadfp):
+        # TODO cleanup flags & init system
         self.verbose, self.duration, self.more_details, self.use_songs, self.analyze_year, self.apikey, self.load, self.loadfp = (verbose, duration, more_details, use_songs, analyze_year, apikey, load, loadfp)
+        
+        # TODO rewrite log system
         self.log = open('log.dat', 'w', encoding="utf8")
+        
         self.main()
         
     def main(self):    
         self.file = self.open_file(sys.argv[1], ".json")
     
-        print("Welcome to YouTube Music Year Wrapper v3.0")
+        # TODO proper file load system
         if self.load:
             print("Loading your preprocessed history file")
             self.songsDF = pd.read_csv(self.open_file(self.loadfp), ".csv")
@@ -29,21 +33,31 @@ class Analyzer():
             print("Removing duplicates")
             self.delete_duplicate()
     
+        # TODO rewrite log system
         if self.verbose:
             self.print_db()
+            
+        # TODO duration code
         if self.duration:
             print("Getting durations. This may take a while.")
             self.get_duration()
+            
+        # TODO more statistics in final report
         print("Getting top 10's")
         self.prepare_tops()
+        
+        # TODO rewrite log system
         if self.verbose:
             self.print_full_tops()
         self.log.close()
+        
+        # TODO update final report
         print("Generating final report")
         self.gen_report()
         print("All done!")
         
     # utility methods
+    # should_not_ignore | Check that a JSON entry meets the parameters for a YTM listening entry in WatchHistory
     def should_not_ignore(self, title, year, header):
         if (header == "YouTube Music" and title[:7] == "Watched" and year[:4] == str(self.analyze_year)):
             return True
@@ -63,7 +77,6 @@ class Analyzer():
     
     def parse_json(self):
         self.songs = {"Title": [], "Artist": [], "Year": [], "URL": []}
-        
         json_object = json.load(self.file)
         for obj in json_object:
             if (self.should_not_ignore(obj['title'], obj['time'], obj['header'])):
@@ -74,7 +87,7 @@ class Analyzer():
                     self.songs["URL"].append(obj['titleUrl'][32:])
     
     def print_db(self):
-        # VERBOSE FUNCTION TBA
+        # TODO rework log system
         pass
     
     def prepare_tops(self):
@@ -82,6 +95,7 @@ class Analyzer():
         self.songs_top = self.songsDF.nlargest(10, ['Occurrences'])
         print("--- TOP SONGS ---")
         print(self.songs_top)
+        
         # Top 10 Artists
         occurrences = {}
         for i, v in enumerate(self.songsDF['Artist']):
@@ -90,14 +104,19 @@ class Analyzer():
             except:
                 print(i, v)
                 occurrences[v] = self.songsDF['Occurrences'][i]
+                
+        # TODO consider moving artist generation to another method
         self.artists = {"Artist": occurrences.keys(), "Occurrences": occurrences.values()}
         del occurrences
         self.artistsDF = pd.DataFrame(self.artists)
         self.artistsDF.to_csv("report-artists.csv")
+        
         self.artists_top = self.artistsDF.nlargest(10, ['Occurrences'])
 
         print("--- TOP ARTISTS ---")
         print(self.artists_top)
+        
+        return songs_top, artists_top
     
     def delete_duplicate(self):
         occurrences = collections.Counter(self.songs['URL'])
@@ -107,18 +126,21 @@ class Analyzer():
         del occurrences
         
         self.historyDF = pd.DataFrame(self.songs)
+        self.historyDF.to_csv("report-history.csv")
+        
         self.songsDF = pd.DataFrame(self.songs)
         self.total_songs = len(self.songsDF)
         self.songsDF.drop_duplicates(subset=['URL'], inplace=True)
         self.unique_songs = len(self.songsDF)
         self.songsDF = self.songsDF.reset_index(drop=True)
-        self.songsDF.to_csv("report-songs.csv")
-    
+        self.songsDF.to_csv("report-songs.csv")    
+        
     def print_full_tops(self):
-        # VERBOSE FUNCTION TBA
+        # TODO rework log system
         pass
     
     # API management functions
+    # TODO clean up this legacy function (?)
     def parse_duration(self, duration):
         timestr = duration
         time = re.findall(r'\d+', timestr)
@@ -137,11 +159,14 @@ class Analyzer():
             return 0
     
     def call_api(self, idlist):
+        # TODO rework log system
         print("api called", file=self.log)
+        
         parameters = {"part": "contentDetails,snippet",
                       "id": ','.join(idlist), "key": self.apikey}
         response = requests.get(
             "https://www.googleapis.com/youtube/v3/videos", params=parameters)
+        
         if (response.status_code == 200):
             json_parsed = response.json()
             for item in json_parsed['items']:
@@ -197,6 +222,9 @@ class Analyzer():
     
     # report generation methods
     def gen_html_report(self):
+        
+        # TODO rewrite HTML report generation
+        
         htmlreport = open('report_{0}.html'.format(
             str(self.analyze_year)), 'w', encoding=("utf8"))
         print("""<!DOCTYPE html><html><head><title>Wrapped</title><style type="text/css">body{background-color: #000000;}.center-div{position: absolute; margin: auto; top: 0; right: 0; bottom: 0; left: 0; width: 50%; height: 90%; background-color: #000000; border-radius: 3px; padding: 10px;}.ytm_logo{width: 15%;position: relative;top: 30px;left: 40px;}.title_logo{width: 30%;position: relative;top: 30px;left: 60px;}.right_title{position: absolute;font-family: "Product Sans";top: 55px;right: 10%;font-size: 2em;color: #ffffff;}.container{position: relative;top: 13%;left: 53px;}.minutes_title{font-family: "Product Sans";font-size: 2em;color: #ffffff;}.minutes{font-family: "Product Sans";font-size: 6em;color: #ffffff;}.row{display: flex;}.column{flex: 50%;}.list{font-family: "Roboto";font-size: 1.5em;line-height: 30px;color: #ffffff;}</style></head><body><div class="center-div"><img src="ytm_logo.png" class="ytm_logo"><img src="title.png" class="title_logo"/><span class="right_title">""", file=htmlreport)
@@ -240,6 +268,9 @@ class Analyzer():
         htmlreport.close()
     
     def gen_report(self):
+        
+        # TODO rewrite text report generation
+        
         # Top 10 Report
         report = open('report_{0}.dat'.format(
             str(self.analyze_year)), 'w', encoding=("utf8"))
@@ -269,6 +300,8 @@ class Analyzer():
         self.gen_html_report()
     
 # flags
+
+# TODO fix parameter/flags system
 verbose, more_details, duration, analyze_year, use_songs, apikey, load, loadfp = (False, False, False, False, datetime.datetime.now().year, None, False, None)
 
 opts, args = getopt.getopt(sys.argv[2:], "d:y:mv", ["duration=", "year="])
